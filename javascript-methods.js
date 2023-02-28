01df7924dfb8ab62d407fa481d2d9d13f6868dd6
Ryan Jiang <jiangr0421@gmail.com>
2023-02-28 17:15:38 -0500

    Added code for mySome

--- javascript-methods.js
+++ javascript-methods.js
@@ -38,7 +38,13 @@ Array.prototype.myFilter = function(callbackFn) {
 
 // SOME //
 Array.prototype.mySome = function(callbackFn) {
-  // Place your code here.
+  for(let i = 0; i < this.length; i++) {
+    if(this[i] === undefined) continue;
+    // element, index, array
+    if(callbackFn(this[i], i, this)) return true;
+  }
+
+  return false;
 };
 
 // EVERY //
